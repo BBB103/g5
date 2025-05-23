b879767466725e68b75f089ce1b2f5c5cb301599
BBB103 <69club221@gmail.com>
2025-05-23 15:56:58 +0800

    description

--- server.py
+++ server.py
@@ -18,6 +18,13 @@ COCKTAILS = {
     "PURPLE": {"name": "紫色", "ingredients": {"red": 15, "blue": 15},"descrption":"紫紫的", "image": "purple.png"}
 }
 
+machine_status= {
+    "busy" : False,
+    "current_cocktail": None,
+    "progress": "待機"
+}
+
+
 @app.route('/')
 def index():
     return send_from_directory('static', 'index.html')
@@ -35,6 +42,29 @@ def handle_order(data):
     print("Forwarding cocktail order to RPi...")
     socketio.emit('start_cocktail', data)  # 發給RPi
     emit('order_received', {'cocktail_id': data.get("cocktail_id")})
+
+@socketio.on("pouring")
+def handle_pour():
+    machine_status["busy"] = True
+    machine_status["progress"] = "正在倒入原料"
+    socketio.emit('status_update', machine_status)
+
+@socketio.on("closing")
+def handle_close():
+    machine_status["busy"] = True
+    machine_status["progress"] = "正在關杯蓋"
+    socketio.emit('status_update', machine_status)
+
+@socketio.on("shaking")
+def handle_shake():
+    machine_status["busy"] = True
+    machine_status["progress"] = "正在=shake"
+    socketio.emit('status_update', machine_status)
+@socketio.on("done")
+def handle_done():
+    machine_status['current_cocktail'] = None
+    machine_status['progress'] = 0
+    socketio.emit('status_update', machine_status)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
