908b658feac6e8920276a0bb50f27acfb1fcb165
BBB103 <69club221@gmail.com>
2025-06-12 02:20:52 +0800

    amount

--- server.py
+++ server.py
@@ -13,31 +13,31 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 COCKTAILS = {
     "RED": {
         "name": "Bloody Mary (紅色)",
-        "ingredients": {"red": 15},
+        "ingredients": {"red": 2},
         "descrption": "番茄汁為基底，鹹香微辣，適合早午餐或解宿醉時飲用，口感濃郁厚實。",
         "image": "Bloody-Mary.jpg"
     },
     "YELLOW": {
-        "name": "Golden Dream(黃色)",
-        "ingredients": {"yellow": 15},
+        "name": "Golden Dream (黃色)",
+        "ingredients": {"yellow": 2},
         "descrption": "由香草酒與柳橙酒調製，果香濃郁甜潤，適合餐後啜飲，酒體柔順層次分明。",
         "image": "screw.jpg"
     },
     "BLUE": {
         "name": "Blue Hawaii (藍色)",
-        "ingredients": {"blue": 15},
+        "ingredients": {"blue": 2},
         "descrption": "藍柑橘搭配蘭姆酒，熱帶風情濃厚，適合派對或海灘時享用，口感清新帶果香。",
         "image": "bluehaw.webp"
     },
     "GREEN": {
         "name": "Grasshopper (綠色)",
-        "ingredients": {"yellow": 15, "blue": 15},
+        "ingredients": {"yellow": 1, "blue": 1},
         "descrption": "薄荷酒與可可酒交織出的甜香，適合飯後作為甜點酒，口感冰涼滑順。",
         "image": "grasshopper.webp"
     },
     "PURPLE": {
         "name": "Aviation   (紫色)",
-        "ingredients": {"red": 15, "blue": 15},
+        "ingredients": {"red": 1, "blue": 1},
         "descrption": "結合琴酒與紫羅蘭香甜酒，適合浪漫夜晚或安靜時刻品飲，口感淡雅略帶花香。",
         "image": "aviation.png"
     }
